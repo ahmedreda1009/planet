1d101f7fecf4ba63d6f693b6a2fefc9ab3c3c72c
ahmed reda <ahmedreda1009@gmail.com>
2023-03-02 17:03:17 +0200

    register and sign up functions.

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,8 @@
 import Swal from 'sweetalert2';
 import axios from 'axios';
 // api url
-const baseUrl = 'https://tarmeezacademy.com/api/v1/';
+// const baseUrl = 'https://tarmeezacademy.com/api/v1/';
+const baseUrl = 'https://reqres.in';
 
 // modal fires when user clicks on the forget password button.
 document.getElementById('forgotten-password').addEventListener('click', () => {
@@ -24,22 +25,69 @@ document.getElementById('close-sign-up').addEventListener('click', () => {
     document.querySelector('.sign-up').classList.remove('active');
 });
 
-let emailLogin = document.getElementById('email-login').value;
-let passwordLogin = document.getElementById('password-login').value;
-
-let nameSignUp = document.getElementById('name-sign-up').value;
-let emailSignUp = document.getElementById('email-sign-up').value;
-let passwordSignUp = document.getElementById('password-sign-up').value;
-let pictureSignUp = document.getElementById('file-sign-up');
-
+// login function
 function login(username, password) {
-    // axios({
-    //     method: 'post',
-    //     url: `${baseUrl}/login`,
-    //     data: {
-    //         email: username,
-    //         password
-    //     }
-    // });
+    axios.post(`${baseUrl}/api/login`, {
+        "email": username,
+        "password": password
+    }).then((response) => {
+        window.localStorage.setItem("token", response.data.token);
+
+        window.location = 'home.html';
+    });
 }
-// axios.get('https://api.aladhan.com/v1/calendarByCity/2017/4?city=Cairo&country=Egypt&method=5').then(data => console.log(data.data.data));
+
+// register function
+function register(username, password) {
+    axios.post(`${baseUrl}/api/register`, {
+        "email": username,
+        "password": password
+    }).then((response) => {
+        console.log(response.data);
+        window.localStorage.setItem("token", response.data.token);
+    });
+}
+
+// trigger login.
+document.getElementById('login-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // email value
+    let emailLogin = document.getElementById('email-login').value;
+    // password value
+    let passwordLogin = document.getElementById('password-login').value;
+
+    login(emailLogin, passwordLogin);
+});
+
+// trigger sign-up.
+document.getElementById('sign-up-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let nameSignUp = document.getElementById('name-sign-up');
+    let emailSignUp = document.getElementById('email-sign-up');
+    let passwordSignUp = document.getElementById('password-sign-up');
+    let pictureSignUpValue = document.getElementById('profile-pic');
+
+    let inputs = [nameSignUp, emailSignUp, passwordSignUp, pictureSignUpValue];
+
+    if (!inputs.find(ele => ele.value == null)) {
+        register(emailSignUp, passwordSignUp);
+        console.log('register');
+    } else {
+        console.log('hi');
+        inputs.map(input => {
+            if (input.value == null) {
+                input.classList.add('empty');
+            }
+        });
+    }
+});
+
+// change file input display image.
+let inputFile = document.getElementById('profile-pic');
+inputFile.addEventListener('change', () => {
+    const image = `<img src=${URL.createObjectURL(inputFile.files[0])} alt="profile picture">`;
+
+    document.getElementById("selected-img").innerHTML = image;
+});
