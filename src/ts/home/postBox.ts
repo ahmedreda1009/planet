b887c797177ff37c7ded68721c3918b4eb577adf
ahmed reda <ahmedreda1009@gmail.com>
2023-03-07 17:16:27 +0200

    added the profile icon.

--- src/ts/home/postBox.ts
+++ src/ts/home/postBox.ts
@@ -1,13 +1,13 @@
 // calculate the height of the text area after writing soem text.
 function calcHeight(value: string): number {
-    let numberOfLineBreaks: number = (value.match(/\n/g) || []).length;
-    // min-height + lines x line-height + padding + border
-    let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
-    return newHeight;
+	let numberOfLineBreaks: number = (value.match(/\n/g) || []).length;
+	// min-height + lines x line-height + padding + border
+	let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+	return newHeight;
 }
 
 // change text area height when writing inside it.
 let textarea = document.getElementById("post-text-area") as HTMLTextAreaElement;
 textarea.addEventListener("keyup", () => {
-    textarea.style.height = calcHeight(textarea.value) + "px";
+	textarea.style.height = calcHeight(textarea.value) + "px";
 });
