e98dbbc2a80d3da345344f9e1990549aa04511ae
Ahmed Reda <ahmedreda1009@gmail.com>
2023-03-08 20:06:59 +0200

    modified the home page widgets.

--- src/ts/home/home.ts
+++ src/ts/home/home.ts
@@ -1,7 +1,7 @@
 import "../../scss/pages/home/_index.scss";
 import '../home/postBox';
 
-let authorized: boolean = true;
+let authorized: boolean = false;
 
 if (!authorized) {
 	window.location.href = "index.html";
