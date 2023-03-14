41b2b4b08bbdfa7635063190e8a25b7b9605f641
ahmed reda <ahmedreda1009@gmail.com>
2023-03-14 16:27:24 +0200

    added the modal of the logout button.

--- src/ts/global.ts
+++ src/ts/global.ts
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 // api url
 // const baseUrl = 'https://tarmeezacademy.com/api/v1/';
 const baseUrl = "https://reqres.in";
@@ -9,7 +11,30 @@ const logoutBtns = document.querySelectorAll('.logout') as NodeList;
 
 logoutBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        window.localStorage.removeItem('token');
-        window.location.href = 'index.html';
+        Swal.fire({
+            title: 'Are you sure?',
+            // text: "We will miss you.",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            // confirmButtonBorderColor: '#000',
+            cancelButtonColor: '#0dcaf0',
+            confirmButtonText: 'Logout!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    {
+                        title: 'logged out successfully!',
+                        showConfirmButton: false,
+                        // 'We will miss you.',
+                        icon: 'info'
+                    })
+
+                setTimeout(() => {
+                    window.localStorage.removeItem('token');
+                    window.location.href = 'index.html';
+                }, 500)
+            }
+        })
     });
 });
